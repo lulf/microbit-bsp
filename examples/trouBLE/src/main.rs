927040c1f79305839986811ba948817e91369dba
James Sizeland <jamessizeland@gmail.com>
2024-11-09 23:37:48 +0000

    working nrf-sdc example from trouble

---
+++ examples/trouBLE/src/main.rs
@@ -0,0 +1,193 @@
+#![no_std]
+#![no_main]
+
+use {defmt_rtt as _, panic_probe as _};
+
+use defmt::{error, info, unwrap};
+use embassy_executor::Spawner;
+use embassy_nrf::peripherals::RNG;
+use embassy_nrf::{bind_interrupts, pac, rng};
+use nrf_sdc::mpsl::MultiprotocolServiceLayer;
+use nrf_sdc::{self as sdc, mpsl};
+use static_cell::StaticCell;
+use embassy_futures::join::join3;
+use embassy_time::{Duration, Timer};
+use trouble_host::prelude::*;
+
+/// Size of L2CAP packets (ATT MTU is this - 4)
+const L2CAP_MTU: usize = 251;
+
+/// Max number of connections
+const CONNECTIONS_MAX: usize = 1;
+
+/// Max number of L2CAP channels.
+const L2CAP_CHANNELS_MAX: usize = 2; // Signal + att
+
+type Resources<C> = HostResources<C, CONNECTIONS_MAX, L2CAP_CHANNELS_MAX, L2CAP_MTU>;
+
+// GATT Server definition
+#[gatt_server(attribute_data_size = 10)]
+struct Server {
+    battery_service: BatteryService,
+}
+
+// Battery service
+#[gatt_service(uuid = "180f")]
+struct BatteryService {
+    #[characteristic(uuid = "2a19", read, notify)]
+    level: u8,
+}
+
+bind_interrupts!(struct Irqs {
+    RNG => rng::InterruptHandler<RNG>;
+    SWI0_EGU0 => nrf_sdc::mpsl::LowPrioInterruptHandler;
+    POWER_CLOCK => nrf_sdc::mpsl::ClockInterruptHandler;
+    RADIO => nrf_sdc::mpsl::HighPrioInterruptHandler;
+    TIMER0 => nrf_sdc::mpsl::HighPrioInterruptHandler;
+    RTC0 => nrf_sdc::mpsl::HighPrioInterruptHandler;
+});
+
+#[embassy_executor::task]
+async fn mpsl_task(mpsl: &'static MultiprotocolServiceLayer<'static>) -> ! {
+    mpsl.run().await
+}
+
+fn build_sdc<'d, const N: usize>(
+    p: nrf_sdc::Peripherals<'d>,
+    rng: &'d mut rng::Rng<RNG>,
+    mpsl: &'d MultiprotocolServiceLayer,
+    mem: &'d mut sdc::Mem<N>,
+) -> Result<nrf_sdc::SoftdeviceController<'d>, nrf_sdc::Error> {
+    sdc::Builder::new()?
+        .support_adv()?
+        .support_peripheral()?
+        .peripheral_count(1)?
+        .build(p, rng, mpsl, mem)
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_nrf::init(Default::default());
+    let pac_p = pac::Peripherals::take().unwrap();
+
+    let mpsl_p = mpsl::Peripherals::new(
+        pac_p.CLOCK,
+        pac_p.RADIO,
+        p.RTC0,
+        p.TIMER0,
+        p.TEMP,
+        p.PPI_CH19,
+        p.PPI_CH30,
+        p.PPI_CH31,
+    );
+    let lfclk_cfg = mpsl::raw::mpsl_clock_lfclk_cfg_t {
+        source: mpsl::raw::MPSL_CLOCK_LF_SRC_RC as u8,
+        rc_ctiv: mpsl::raw::MPSL_RECOMMENDED_RC_CTIV as u8,
+        rc_temp_ctiv: mpsl::raw::MPSL_RECOMMENDED_RC_TEMP_CTIV as u8,
+        accuracy_ppm: mpsl::raw::MPSL_DEFAULT_CLOCK_ACCURACY_PPM as u16,
+        skip_wait_lfclk_started: mpsl::raw::MPSL_DEFAULT_SKIP_WAIT_LFCLK_STARTED != 0,
+    };
+    static MPSL: StaticCell<MultiprotocolServiceLayer> = StaticCell::new();
+    let mpsl = MPSL.init(unwrap!(mpsl::MultiprotocolServiceLayer::new(mpsl_p, Irqs, lfclk_cfg)));
+    spawner.must_spawn(mpsl_task(&*mpsl));
+
+    let sdc_p = sdc::Peripherals::new(
+        pac_p.ECB, pac_p.AAR, p.PPI_CH17, p.PPI_CH18, p.PPI_CH20, p.PPI_CH21, p.PPI_CH22, p.PPI_CH23, p.PPI_CH24,
+        p.PPI_CH25, p.PPI_CH26, p.PPI_CH27, p.PPI_CH28, p.PPI_CH29,
+    );
+
+    let mut rng = rng::Rng::new(p.RNG, Irqs);
+
+    let mut sdc_mem = sdc::Mem::<3312>::new();
+    let sdc = unwrap!(build_sdc(sdc_p, &mut rng, mpsl, &mut sdc_mem));
+
+    run(sdc).await;
+}
+
+pub async fn run<C>(controller: C)
+where
+    C: Controller,
+{
+    let address = Address::random([0x41, 0x5A, 0xE3, 0x1E, 0x83, 0xE7]);
+    info!("Our address = {:?}", address);
+
+    let mut resources = Resources::new(PacketQos::None);
+    let (stack, peripheral, _, runner) = trouble_host::new(controller, &mut resources)
+        .set_random_address(address)
+        .build();
+
+    let server = Server::new_with_config(
+        stack,
+        GapConfig::Peripheral(PeripheralConfig {
+            name: "TrouBLE",
+            appearance: &appearance::GENERIC_POWER,
+        }),
+    );
+
+    info!("Starting advertising and GATT service");
+    let _ = join3(
+        ble_task(runner),
+        gatt_task(&server),
+        advertise_task(peripheral, &server),
+    )
+    .await;
+}
+
+async fn ble_task<C: Controller>(mut runner: Runner<'_, C>) -> Result<(), BleHostError<C::Error>> {
+    runner.run().await
+}
+
+async fn gatt_task<C: Controller>(server: &Server<'_,'_, C>) {
+    loop {
+        match server.next().await {
+            Ok(GattEvent::Write { handle, connection: _ }) => {
+                let _ = server.get(handle, |value| {
+                    info!("[gatt] Write event on {:?}. Value written: {:?}", handle, value);
+                });
+            }
+            Ok(GattEvent::Read { handle, connection: _ }) => {
+                info!("[gatt] Read event on {:?}", handle);
+            }
+            Err(e) => {
+                error!("[gatt] Error processing GATT events: {:?}", e);
+            }
+        }
+    }
+}
+
+async fn advertise_task<C: Controller>(
+    mut peripheral: Peripheral<'_, C>,
+    server: &Server<'_, '_, C>,
+) -> Result<(), BleHostError<C::Error>> {
+    let mut adv_data = [0; 31];
+    AdStructure::encode_slice(
+        &[
+            AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
+            AdStructure::ServiceUuids16(&[Uuid::Uuid16([0x0f, 0x18])]),
+            AdStructure::CompleteLocalName(b"Trouble"),
+        ],
+        &mut adv_data[..],
+    )?;
+    loop {
+        info!("[adv] advertising");
+        let mut advertiser = peripheral
+            .advertise(
+                &Default::default(),
+                Advertisement::ConnectableScannableUndirected {
+                    adv_data: &adv_data[..],
+                    scan_data: &[],
+                },
+            )
+            .await?;
+        let conn = advertiser.accept().await?;
+        info!("[adv] connection established");
+        // Keep connection alive
+        let mut tick: u8 = 0;
+        while conn.is_connected() {
+            Timer::after(Duration::from_secs(2)).await;
+            tick = tick.wrapping_add(1);
+            info!("[adv] notifying connection of tick {}", tick);
+            let _ = server.notify(server.battery_service.level, &conn, &[tick]).await;
+        }
+    }
+}
